5b16bed48527be8a6bd2927024d8d34ecc1fe703
Anshul Bharat <anshul.ab17x@gmail.com>
2026-05-01 21:42:26 +0530

    readme

--- bot/orders.py
+++ bot/orders.py
@@ -1,8 +1,11 @@
 import logging
+import time
 
 def place_order(client, symbol, side, order_type, quantity, price=None):
     try:
-        logging.info(f"Order request: {symbol} {side} {order_type} qty={quantity} price={price}")
+        logging.info(
+            f"Order request: {symbol} {side} {order_type} qty={quantity} price={price}"
+        )
 
         params = {
             "symbol": symbol,
@@ -11,20 +14,31 @@ def place_order(client, symbol, side, order_type, quantity, price=None):
             "quantity": quantity
         }
 
-        # LIMIT
+        # LIMIT order
         if order_type == "LIMIT":
             params["price"] = price
             params["timeInForce"] = "GTC"
 
-        # STOP_MARKET
+        # STOP_MARKET  
         elif order_type == "STOP_MARKET":
             params["stopPrice"] = price
             params["timeInForce"] = "GTC"
-
+ 
         response = client.futures_create_order(**params)
+        logging.info(f"Initial response: {response}")
 
-        logging.info(f"Order response: {response}")
-        return response
+        
+        order_id = response["orderId"]
+        time.sleep(1)
+
+        updated_order = client.futures_get_order(
+            symbol=symbol,
+            orderId=order_id
+        )
+
+        logging.info(f"Updated order: {updated_order}")
+
+        return updated_order
 
     except Exception as e:
         logging.error(f"Order error: {str(e)}")
