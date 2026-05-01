7094fca8709808bb52c9415cfdd72e2f47d343b3
Anshul Bharat <anshul.ab17x@gmail.com>
2026-05-01 21:03:59 +0530

    added validation

--- bot/client.py
+++ bot/client.py
@@ -2,3 +2,11 @@ import os
 from binance.client import Client
 from dotenv import load_dotenv
 
+load_dotenv()
+
+def get_client():
+    return Client(
+        os.getenv("BINANCE_API_KEY"),
+        os.getenv("BINANCE_API_SECRET"),
+        testnet=True
+    )
